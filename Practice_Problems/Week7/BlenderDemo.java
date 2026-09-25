903c7bdbebe173f829944c485823ffa4e1d72094
anamikag9641-svg <anamikag9641@gmail.com>
2026-09-25 19:53:09 +0530

    Add Week 7 practice problems

---
+++ Practice_Problems/Week7/BlenderDemo.java
@@ -0,0 +1,54 @@
+abstract class KitchenTool {
+
+    private int speedLevel;
+
+    public abstract String prepare();
+
+    public int getSpeedLevel() {
+        return speedLevel;
+    }
+
+    public void setSpeedLevel(int speedLevel) {
+
+        if (speedLevel >= 1 && speedLevel <= 5) {
+            this.speedLevel = speedLevel;
+        }
+    }
+}
+
+interface Washable {
+
+    String clean();
+}
+
+class Blender extends KitchenTool implements Washable {
+
+    @Override
+    public String prepare() {
+        return "Blending at speed " + getSpeedLevel();
+    }
+
+    @Override
+    public String clean() {
+        return "Blender rinsed and dried";
+    }
+}
+
+public class BlenderDemo {
+
+    public static void main(String[] args) {
+
+        Blender b = new Blender();
+
+        b.setSpeedLevel(3);
+
+        System.out.println(b.getSpeedLevel());
+
+        b.setSpeedLevel(9);
+
+        System.out.println(b.getSpeedLevel());
+
+        System.out.println(b.prepare());
+        System.out.println(b.clean());
+    }
+}
